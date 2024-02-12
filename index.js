b23b2d9012e7d24e4f84aaa114cf8fbace51f278
RebelsBoss <126337643+RebelsBoss@users.noreply.github.com>
2024-02-12 09:18:58 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,51 @@
 const http = require('http');
- 
-const server = http.createServer((req, res) => {
-  const serverIp = req.socket.remoteAddress; // Отримання IP-адреси сервера
- 
-  res.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
-  res.end(`
-<!DOCTYPE html>
-<html lang="uk">
-<head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>IP адреса сервера</title>
-</head>
-<body>
-<h1>Ваша IP адреса: ${req.socket.remoteAddress}</h1>
-<h2>IP адреса сервера: ${serverIp}</h2>
-</body>
-</html>
-    `);
-});
- 
-server.listen(8080, () => {
-  console.log('** Сервер запущен на порту 8080 **');
+const port = 8080;
+
+const index_page = `
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Hello World!</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <style>
+        .container { min-height: 100vh; display: flex; justify-content: center; align-items: center; text-align: center; }
+        .title { font-family: "Source Sans Pro", "Helvetica Neue", Arial, sans-serif; display: block; font-weight: 300; font-size: 100px; color: #35495e; letter-spacing: 1px; }
+        .subtitle { font-family: "Source Sans Pro", "Helvetica Neue", Arial, sans-serif; font-weight: 300; font-size: 42px; color: #526488; word-spacing: 5px; padding-bottom: 15px; }
+        .links { padding-top: 15px; }
+    </style>
+  </head>
+  <body>
+    <section class="container">
+      <div>
+        <h1 class="title">
+        👋 Hello World 🌍😀
+        </h1>
+        <h2 class="subtitle">
+        made with 💚 and 🍵
+        </h2>
+        <h2 class="subtitle">
+        DevOps top
+        </h2>                
+      </div>
+    </section>
+  </body>
+</html>  
+`;
+
+const requestHandler = (request, response) => {
+  console.log(request.url);
+  response.writeHead(200, {'Content-Type': 'text/html; charset=utf-8'});
+  response.end(index_page);
+};
+
+const server = http.createServer(requestHandler);
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('😡 something bad happened', err);
+  }
+  console.log(`🌍 server is listening on ${port}`);
 });
